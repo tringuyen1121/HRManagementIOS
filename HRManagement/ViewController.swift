e1d1d30086261fdc6dd267616588da5a556cc07b
Tri Nguyen <tri.nguyen@happeo.com>
2019-09-13 23:05:19 +0300

    Initial Commit

---
+++ HRManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HRManagement
+//
+//  Created by Tri Nguyen on 13/09/2019.
+//  Copyright © 2019 Tri Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
